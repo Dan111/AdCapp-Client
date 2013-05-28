86b5169036c5729a011d564c09971f34deee6063
Dan_111 <dmagro04@gmail.com>
2013-05-28 20:00:26 +0100

    Pesquisa Agenda

--- app/collections/events.js
+++ app/collections/events.js
@@ -16,12 +16,12 @@ function ($, Backbone, LocalStorage, Event) {
 		//localStorage: new Backbone.LocalStorage('events-backbone'),
 
 		initialize: function (){
-			
-			//this.url = "http://danielmagro.apiary.io/events";
 			console.log('EventS');
 		},
 
-		 getEventByName: function(name){
+		//Devolve o evento com um dado name
+		//caso contrário devolve null
+		getEventByName: function(name){
             
             return  this.find( function(event_obj){ 
             	if(event_obj.get("name") === name)
@@ -29,6 +29,69 @@ function ($, Backbone, LocalStorage, Event) {
             	else
             		return null;
             });
+        },
+
+        //Filtra a collection devolvendo um array com os 
+        //objectos que teem nome a stinr passada
+        getEventsWithString: function(string){
+            var lowerString = string.toLowerCase();
+            return this.filter(function(event_obj) {
+                    return event_obj.get('name').toLowerCase().indexOf(lowerString) > -1;
+                });
+        },
+
+        //Filtra a collection devolvendo um array com os
+        //objectos com os tipos desejados
+        //São passados um objecto javascript do tipo
+        //{papers: boolean, workshops: boolean, socials: boolean}
+        //e um número para se saber o número de tipos requeridos
+        getEventsOfType: function(types, numberOfTypes){
+
+        	var type1 = null;
+        	var type2 = null;
+        	var type3 = null;
+
+        	//verificamos quais os tipos que
+        	//são necessários
+        	if (types.papers)
+        		type1 = "paper";
+
+        	if (types.workshops)
+        	{	
+        		if(type1 === null)
+        			type1 = "workshop";
+        		else
+        			type2 = "workshop";
+        	}
+
+        	if (types.socials)
+        	{
+        		if(type1 === null)
+        			type1 = "social";
+        		else if(type2 === null)
+        			type2 = "social";
+        		else
+        			type3 = "social";
+        	}
+
+        	//Se um dos type fosse a null funcionava, mas
+        	// para não haver nenhum erro, já que o default do
+        	// type de um event é null, decidi usar o tal
+        	//numberOfTypes
+        	if(numberOfTypes === 1)
+	        	return this.filter(function(event_obj) {
+					return event_obj.get('type') === type1;
+				});
+	        else if(numberOfTypes === 2)
+	        	return this.filter(function(event_obj) {
+	        		var type = event_obj.get('type')
+					return  type === type1 || type === type2;
+				});
+	        else 
+	        	return this.filter(function(event_obj) {
+	        		var type = event_obj.get('type')
+					return  type === type1 || type === type2 || type === type3;
+				});
         }
 
 
--- app/views/agenda.js
+++ app/views/agenda.js
@@ -1,16 +1,19 @@
 define([
     "jquery",
     "backbone",
+    "underscore",
     "handlebars",
     "fullcalendar",
     "moment",
     "collections/events",
     "views/basicview"
-], function ($, Backbone, Handlebars, FullCalendar, Moment, EventCollection, BasicView) {
+], 
+
+function ($, Backbone, _, Handlebars, FullCalendar, Moment, EventCollection, BasicView) {
 
 	return BasicView.extend({
 
-		el: "div[data-role=content]",
+		//el: "div[data-role=content]",
 
 		id: "agenda-page",
 		pageName: "Agenda",
@@ -18,13 +21,25 @@ define([
 		template: "agenda-template",
 
 		events: {
-			"click #popupbutton" :  "popup",
+			'click #popupbutton' :  'popup',
+			'click #searchsubmit' : 'search',
 			'click #my-prev' : 'prev',
 			'click #my-next' : 'next',
 			'click #my-today' : 'today'
 		},
 
 		$calendar: null,
+		$teacherlink: null,
+		$authorlink: null,
+		$teacherlinkA: null,
+		$authorlinkA: null,
+		$eventlinkA: null,
+		$locallinkA: null,
+		$searchbasic: null,
+		$paperscheck: null,
+		$workshopscheck: null,
+		$socialscheck: null,
+
 
 		initialize: function ()
 		{
@@ -34,8 +49,8 @@ define([
 			
 
 			this.conferenceEvents = new EventCollection();
-			console.log(this.conferenceEvents);
-
+			
+			
 			this.conferenceEvents.fetch({
 				success: function () {
 					self.renderLayout();
@@ -44,8 +59,7 @@ define([
 				}
 			});
 
-			//this.$calendar = $("#calendar");
-			
+
 		},
 
 
@@ -79,8 +93,20 @@ define([
 
 			this.setElement($("[data-role=content]"));
 
-			var treatedEvents = this.conferenceEvents.map(this.treatEvents);
 
+			this.fullCalendarSetter(this.conferenceEvents);
+
+		
+
+			return this;
+
+		},
+
+		fullCalendarSetter: function(renderEvents)
+		{
+			var selectedEvents = renderEvents || this.conferenceEvents;
+
+			var treatedEvents = selectedEvents.map(this.treatEvents);
 
 			$(document).ready(function() {
 				
@@ -107,18 +133,27 @@ define([
 
 				       	that.setPopUp(attributes);
 
-	    				$('#popupMenu').popup('open', { positionTo: this, transition: "slideup" });
+	    				$('#popupMenu').popup('open', { positionTo: this });
 	    				
 				   }
 				});
 				
-				$('.fc-event-inner').attr("id", "#popupbutton");
+				
 			});
 
-			this.$calendar = $('#calendar');
-			
 
-			return this;
+
+			this.$calendar = $('#calendar');
+			this.$teacherlink = $('#teacher-link');
+			this.$authorlink = $('#author-link');
+			this.$teacherlinkA = $('#teacher-link a');
+			this.$authorlinkA = $('#author-link a');
+			this.$eventlinkA = $('#event-link a');
+			this.$locallinkA = $('#local-link a');
+			this.$searchbasic = $("#search-basic");
+			this.$paperscheck = $("#paperscheck");
+			this.$workshopscheck = $("#workshopscheck");
+			this.$socialscheck = $("#socialscheck");
 
 		},
 
@@ -126,33 +161,66 @@ define([
 		setPopUp: function(attributes){
 			var type = attributes.type;
 
-			
 			if(type === "paper")
 			{
-				$('#teacher-link').hide();
-				$('#author-link a').attr("href", "#user/" + attributes.users_id_array[0]);
-				$('#author-link').show();
+				this.$teacherlink.hide();
+				this.$authorlinkA.attr("href", "#user/" + attributes.users_id_array[0]);
+				this.$authorlink.show();
 			}
 			else if(type === "workshop")
 			{
-				$('#teacher-link a').attr("href", "#user/" + attributes.users_id_array[0]);
-				$('#teacher-link').show();
-				$('#author-link').hide();
+				this.$teacherlinkA.attr("href", "#user/" + attributes.users_id_array[0]);
+				this.$teacherlink.show();
+				this.$authorlink.hide();
 			}
 			else
 			{
-				$('#teacher-link').hide();
-				$('#author-link').hide();
+				this.$teacherlink.hide();
+				this.$authorlink.hide();
 			}
 
-			$('#event-link a').attr("href", "#paper/" + attributes.id);
-			$('#local-link a').attr("href", "#local/" +  attributes.local_id);
+			this.$eventlinkA.attr("href", "#paper/" + attributes.id);
+			this.$locallinkA.attr("href", "#local/" +  attributes.local_id);
 		},
 
-		popup: function (e){
+		popup: function(e) {
    			e.preventDefault();
 
    			$('#popupMenu').popup('open', { positionTo: "#popupbutton", transition: "slideup" });
+
+  		},
+
+  		search: function() {
+  			var terms = this.$searchbasic.val().trim();
+  			var counter = 0;
+  			var papers = this.$paperscheck.is(':checked');
+  			var workshops= this.$workshopscheck.is(':checked');
+  			var socials = this.$socialscheck.is(':checked');
+  			var types = {papers: papers, workshops: workshops, socials: socials};
+
+  			if(papers)
+  				counter+=1;
+
+  			if(workshops)
+  				counter+=1;
+
+  			if(socials)
+  				counter+=1;
+  			console.log(terms);
+  			var stringResults = this.conferenceEvents.getEventsWithString(terms);
+  			var typeResults = [];
+  			var renderEvents = [];
+
+  			if(counter>0)
+  			{	
+  				typeResults = this.conferenceEvents.getEventsOfType(types, counter);		
+  			  	renderEvents = _.intersection(stringResults, typeResults);
+  			}
+  			else
+  				renderEvents = stringResults;
+  			
+  			this.$calendar.empty();
+  			this.fullCalendarSetter(renderEvents);
 
   		},
 
