3f0abcc71fcd90aab562e56bf2890793c9d8a808
Dan_111 <dmagro04@gmail.com>
2013-06-18 12:49:52 +0100

    Pesquisa geral completa

---
+++ app/collections/locals.js
@@ -0,0 +1,55 @@
+define([
+    "jquery",
+    "backbone",
+    "backbone.localStorage",
+    "models/local",
+    "app.config"
+], 
+
+function ($, Backbone, LocalStorage, Local, App) {
+
+	/**
+    Coleção de locais
+
+    @class LocalsCollection
+    @extends Backbone.Collection
+    **/
+	return Backbone.Collection.extend({
+
+		/**
+        Tipo do modelo utilizado na collection
+
+        @property model 
+        @type Backbone.Model
+        @final
+        @protected
+        @default Local
+        **/
+		model: Local,
+
+		url: App.URL + "locals",
+
+
+		initialize: function (){
+		},
+
+	    /**
+        Filtra a collection devolvendo um array com os 
+        objectos que teem no nome a string passada
+
+        @method getEventsWithString
+        @protected
+        @param {String} string termo de filtragem
+        @return {Array} array de modelos Local
+        **/
+        getLocalsWithString: function(string){
+            var lowerString = string.toLowerCase();
+            return this.filter(function(event_obj) {
+                    return event_obj.get('name').toLowerCase().indexOf(lowerString) > -1;
+                });
+        },
+
+
+	});
+
+});
